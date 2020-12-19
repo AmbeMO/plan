2cdae6058b4f187a663e7e33b6e742a490df4669
AmbeMO <46319210+AmbeMO@users.noreply.github.com>
2020-12-19 16:35:02 +0300

    add indicators

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class sportManagingApp extends StatelessWidget{
       title: 'sport Managing App',
       theme: ThemeData(
         primaryColor: Color.fromRGBO(195, 154, 219, 1),
-        textTheme: TextTheme(title: TextStyle(color: Colors.white))
+        textTheme: TextTheme(title: TextStyle(color: Colors.purple))
       ),
       home: HomePage(),
 
@@ -39,8 +39,8 @@ class WorkoutsList extends StatelessWidget{
     Workout(title: 'Test1',author: 'Milana',description: 'Test Workout1',level: 'Beginner'),
     Workout(title: 'Test2',author: 'Milana',description: 'Test Workout2',level: 'Intermidiate'),
     Workout(title: 'Test3',author: 'Milana',description: 'Test Workout3',level: 'Advanced'),
-    Workout(title: 'Test4',author: 'Milana',description: 'Test Workout4',level: 'sub-master'),
-    Workout(title: 'Test5',author: 'Milana',description: 'Test Workout5',level: 'Proffesional'),
+    Workout(title: 'Test4',author: 'Milana',description: 'Test Workout4',level: 'Sub-master'),
+    Workout(title: 'Test5',author: 'Milana',description: 'Test Workout5',level: 'Master'),
   ];
 
   @override
@@ -54,8 +54,19 @@ class WorkoutsList extends StatelessWidget{
               elevation: 2.0,
               margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
               child: Container(
+                decoration: BoxDecoration(color: Color.fromRGBO(195, 154, 219, 0.8) ),
                 child: ListTile(
-                  title: Text(workouts[i].title),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                  leading: Container(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Icon(Icons.fitness_center, color: Theme.of(context).textTheme.title.color),
+                    decoration: BoxDecoration(
+                      border: Border(right: BorderSide(width: 1, color: Colors.white24))
+                    ),
+                  ),
+                  title: Text(workouts[i].title, style: TextStyle(color: Theme.of(context).textTheme.title.color, fontWeight: FontWeight.bold)),
+                  trailing: Icon(Icons.keyboard_arrow_right, color: Theme.of(context).textTheme.title.color),
+                  subtitle: subtitle(context, workouts[i]),
                 ),
               ),
             );
@@ -64,4 +75,54 @@ class WorkoutsList extends StatelessWidget{
       ),
     );
   }
+}
+
+Widget subtitle(BuildContext context, Workout workout){
+  var color = Colors.grey;
+  double indicatorLevel = 0;
+
+  switch(workout.level){
+    case 'Beginner':
+      color = Colors.lightGreen;
+      indicatorLevel = 0.20;
+      break;
+
+    case 'Intermidiate':
+      color = Colors.green;
+      indicatorLevel = 0.40;
+      break;
+
+    case 'Advanced':
+      color = Colors.orange;
+      indicatorLevel = 0.60;
+      break;
+
+    case 'Sub-master':
+      color = Colors.yellow;
+      indicatorLevel = 0.80;
+      break;
+
+    case 'Master':
+      color = Colors.red;
+      indicatorLevel = 1;
+      break;
+  }
+
+  return Row(
+    children: <Widget>[
+      Expanded(
+        flex: 1,
+        child: LinearProgressIndicator(
+          backgroundColor: Theme.of(context).textTheme.title.color,
+          value: indicatorLevel,
+          valueColor: AlwaysStoppedAnimation(color)
+        ),
+      ),
+      SizedBox(width: 10),
+      Expanded(
+        flex: 3,
+        child: Text(workout.level, style: TextStyle(color: Theme.of(context).textTheme.title.color))
+      )
+    ],
+  );
 }
